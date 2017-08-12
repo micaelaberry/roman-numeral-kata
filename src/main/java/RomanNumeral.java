89a6361b3d9e6f129f110a34f7309f836c3823dc
micaela berry <msberry9@gmail.com>
2017-08-12 16:50:40 -0400

    refactored

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -3,17 +3,17 @@ public class RomanNumeral {
 
 	public static String arabicToRoman(int arabic) {
 		  StringBuilder result = new StringBuilder();
-		  if (arabic == 6){
-			  return "VI";
-		  }
+		  int numeral = arabic;
 		  
-		  else if (arabic == 5){
-			  return "V";
+		  if (numeral >= 5){
+			  result.append ("V");
+			  numeral -= 5;
 		  }
-		  else if (arabic == 4) {
-		    return "IV";
+		  if (numeral >= 4) {
+			  result.append ("IV");
+			  numeral -= 4;
 		  }
-		  for (int number = 0; number < arabic; number++) {
+		  for (int count = 0; count < numeral; count++) {
 		    result.append("I");
 		  }
 		  return result.toString();
